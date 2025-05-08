1a697a2004a5cae0fb2cba7e38aa45de3e36672a
hwidung <hwhwhw74@naver.com>
2025-05-08 22:11:52 +0900

    [Feat] iOS 버전 수정 및 VC 테스트

--- Uber-iOS/Presentation/ViewController.swift
+++ Uber-iOS/Presentation/ViewController.swift
@@ -5,15 +5,27 @@
 //  Created by 조휘원 on 5/8/25.
 //
 
+import SnapKit
+import Then
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    
-}
 
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(helloLabel)
+
+        helloLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+
+    private let helloLabel = UILabel().then {
+        $0.text = "우버들아 안뇽 붕붕"
+        $0.textColor = .black
+        $0.font = .systemFont(ofSize: 24, weight: .bold)
+        $0.textAlignment = .center
+    }
+}
