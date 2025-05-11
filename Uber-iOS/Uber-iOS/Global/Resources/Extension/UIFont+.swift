fff1e80b919c40bff8847b230249a50401d7038c
SunyoungJu <sts07190@naver.com>
2025-05-11 21:10:19 +0900

    [Fix] UIFont 익스텐션 강제 언래핑 제거 및 fallback 처리 (#3)

--- Uber-iOS/Uber-iOS/Global/Resources/Extension/UIFont+.swift
+++ Uber-iOS/Uber-iOS/Global/Resources/Extension/UIFont+.swift
@@ -9,31 +9,31 @@ import UIKit
 extension UIFont {
 
     // MARK: - Head
-    static let head_eb48 = UIFont(name: "SUIT-ExtraBold", size: 48)!
+    static let head_eb48 = UIFont(name: "SUIT-ExtraBold", size: 48) ?? UIFont.systemFont(ofSize: 48, weight: .heavy)
 
     // MARK: - Title
-    static let title1_eb32 = UIFont(name: "SUIT-ExtraBold", size: 32)!
-    static let title2_sb22 = UIFont(name: "SUIT-SemiBold", size: 22)!
-    static let title3_eb20 = UIFont(name: "SUIT-ExtraBold", size: 20)!
-    static let title3_b20 = UIFont(name: "SUIT-Bold", size: 20)!
+    static let title1_eb32 = UIFont(name: "SUIT-ExtraBold", size: 32) ?? UIFont.systemFont(ofSize: 32, weight: .heavy)
+    static let title2_sb22 = UIFont(name: "SUIT-SemiBold", size: 22) ?? UIFont.systemFont(ofSize: 22, weight: .semibold)
+    static let title3_eb20 = UIFont(name: "SUIT-ExtraBold", size: 20) ?? UIFont.systemFont(ofSize: 20, weight: .heavy)
+    static let title3_b20 = UIFont(name: "SUIT-Bold", size: 20) ?? UIFont.systemFont(ofSize: 20, weight: .bold)
 
     // MARK: - Body 1
-    static let body1_eb18 = UIFont(name: "SUIT-ExtraBold", size: 18)!
-    static let body1_b18 = UIFont(name: "SUIT-Bold", size: 18)!
-    static let body1_sb18 = UIFont(name: "SUIT-SemiBold", size: 18)!
+    static let body1_eb18 = UIFont(name: "SUIT-ExtraBold", size: 18) ?? UIFont.systemFont(ofSize: 18, weight: .heavy)
+    static let body1_b18 = UIFont(name: "SUIT-Bold", size: 18) ?? UIFont.systemFont(ofSize: 18, weight: .bold)
+    static let body1_sb18 = UIFont(name: "SUIT-SemiBold", size: 18) ?? UIFont.systemFont(ofSize: 18, weight: .semibold)
 
     // MARK: - Body 2
-    static let body2_eb16 = UIFont(name: "SUIT-ExtraBold", size: 16)!
-    static let body2_sb16 = UIFont(name: "SUIT-SemiBold", size: 16)!
-    static let body2_m16 = UIFont(name: "SUIT-Medium", size: 16)!
+    static let body2_eb16 = UIFont(name: "SUIT-ExtraBold", size: 16) ?? UIFont.systemFont(ofSize: 16, weight: .heavy)
+    static let body2_sb16 = UIFont(name: "SUIT-SemiBold", size: 16) ?? UIFont.systemFont(ofSize: 16, weight: .semibold)
+    static let body2_m16 = UIFont(name: "SUIT-Medium", size: 16) ?? UIFont.systemFont(ofSize: 16, weight: .medium)
 
     // MARK: - Body 3
-    static let body3_b14 = UIFont(name: "SUIT-Bold", size: 14)!
-    static let body3_sb14 = UIFont(name: "SUIT-SemiBold", size: 14)!
-    static let body3_m14 = UIFont(name: "SUIT-Medium", size: 14)!
+    static let body3_b14 = UIFont(name: "SUIT-Bold", size: 14) ?? UIFont.systemFont(ofSize: 14, weight: .bold)
+    static let body3_sb14 = UIFont(name: "SUIT-SemiBold", size: 14) ?? UIFont.systemFont(ofSize: 14, weight: .semibold)
+    static let body3_m14 = UIFont(name: "SUIT-Medium", size: 14) ?? UIFont.systemFont(ofSize: 14, weight: .medium)
 
     // MARK: - Caption
-    static let caption_b12 = UIFont(name: "SUIT-Bold", size: 12)!
-    static let caption_sb12 = UIFont(name: "SUIT-SemiBold", size: 12)!
-    static let caption_m12 = UIFont(name: "SUIT-Medium", size: 12)!
+    static let caption_b12 = UIFont(name: "SUIT-Bold", size: 12) ?? UIFont.systemFont(ofSize: 12, weight: .bold)
+    static let caption_sb12 = UIFont(name: "SUIT-SemiBold", size: 12) ?? UIFont.systemFont(ofSize: 12, weight: .semibold)
+    static let caption_m12 = UIFont(name: "SUIT-Medium", size: 12) ?? UIFont.systemFont(ofSize: 12, weight: .medium)
 }
