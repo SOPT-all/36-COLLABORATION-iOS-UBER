a3b065b26fcc267ce49c95337294608d37b13afc
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-05-14 00:18:41 +0900

    [Add] 예약 정보 화면(ReservationViewController) 추가 (#12)

---
+++ Uber-iOS/Uber-iOS/Presentation/ReservationInfoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ReservationInfoViewController.swift
+//  Uber-iOS
+//
+//  Created by 권석기 on 5/14/25.
+//
+
+import UIKit
+
+import SnapKit
+
+// MARK: - Properties
+
+final class ReservationInfoViewController: UIViewController {
+    
+}
+
+// MARK: - LifeCycle
+
+extension ReservationInfoViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
