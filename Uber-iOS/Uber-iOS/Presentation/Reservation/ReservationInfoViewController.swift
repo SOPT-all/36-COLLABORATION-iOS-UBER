09830ba7fcbe6e03f7a4e0c39e1172c76c137808
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-05-14 00:38:32 +0900

    [Add] SectionView 추가 (#12)

---
+++ Uber-iOS/Uber-iOS/Presentation/Reservation/ReservationInfoViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ReservationInfoViewController.swift
+//  Uber-iOS
+//
+//  Created by 권석기 on 5/14/25.
+//
+
+import UIKit
+
+import SnapKit
+
+// MARK: - Properties
+
+final class ReservationInfoViewController: UIViewController {
+    
+    private let scrollView = UIScrollView().then {
+        $0.backgroundColor = .red
+    }
+    
+    private let contentStackView = UIStackView().then {
+        $0.backgroundColor = .bgGray
+        $0.spacing = 8
+    }
+}
+
+// MARK: - LifeCycle
+
+extension ReservationInfoViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubViews()
+        setLayout()
+    }
+}
+
+// MARK: - Layout
+
+extension ReservationInfoViewController {
+    private func addSubViews() {
+        scrollView.addSubview(contentStackView)
+        [scrollView].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    private func setLayout() {
+        scrollView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        contentStackView.snp.makeConstraints {
+            $0.edges.equalTo(scrollView.contentLayoutGuide)
+            $0.width.equalTo(scrollView.contentLayoutGuide)
+        }
+    }
+}
+
+#Preview {
+    ReservationInfoViewController()
+}
---
+++ Uber-iOS/Uber-iOS/Presentation/Reservation/SectionView.swift
@@ -0,0 +1,20 @@
+//
+//  SectionView.swift
+//  Uber-iOS
+//
+//  Created by 권석기 on 5/14/25.
+//
+
+import UIKit
+
+class SectionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
