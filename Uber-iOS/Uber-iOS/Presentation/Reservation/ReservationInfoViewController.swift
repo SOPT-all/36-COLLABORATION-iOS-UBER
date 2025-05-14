e403e125cf142b61c6b9f2c3b9eb7ac0c80ef3c9
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-05-15 03:15:59 +0900

    [Feat] 예약 정보 화면 UI 구현 (#12) - 예약 정보 화면 UI  구현 - SectionView 재사용성 개선 관련 Extension 추가

--- Uber-iOS/Uber-iOS/Presentation/Reservation/ReservationInfoViewController.swift
+++ Uber-iOS/Uber-iOS/Presentation/Reservation/ReservationInfoViewController.swift
@@ -15,35 +15,144 @@ final class ReservationInfoViewController: UIViewController {
     
     // Content
     
+    private let imageView = UIImageView().then {
+        $0.image = UIImage(resource: .route)
+    }
+    
     private let pickupTimeLabel = UILabel().then {
         $0.font = .caption_m12
         $0.textColor = .sub1
         $0.attributedText = "05월 05일 (월) / 오전 20:16".replaceFont(pattern: "[0-9]|\\([ㄱ-ㅣ가-힣]\\)", replaceFont: .body2_sb16)
     }
     
-    // Sections
+    private let expectedArriveLabel = UILabel().then {
+        $0.font = .caption_m12
+        $0.textColor = .sub1
+        $0.attributedText = "오전 20:31".replaceFont(pattern: "[0-9]|\\([ㄱ-ㅣ가-힣]\\)", replaceFont: .body2_sb16)
+    }
     
-    private let startAndArriveSection = SectionView(title: "출발/도착", content: UIView())
+    private let expectedArriveDetailLabel = UILabel().then {
+        $0.font = .caption_m12
+        $0.textColor = .sub1
+        $0.text = "예상 운행 시간 약 25분 소요 예상"
+    }
     
-    private lazy var pickupTimeSection = SectionView(title: "픽업 시간", content: pickupTimeLabel, contentEdge: .init(top: 0, left: 25, bottom: 0, right: 25))
+    private lazy var expectedArriveStack = UIStackView().then {
+        $0.axis = .vertical
+        $0.alignment = .leading
+        $0.addArrangedSubviews(expectedArriveLabel, expectedArriveDetailLabel)
+    }
     
-    private let expectedArriveSection = SectionView(title: "예정 도착 시간", content: UIView())
+    private lazy var vehicleSelectionButton = UIButton().then {
+        $0.setTitle("차량 선택하기", for: .normal)
+        $0.setTitleColor(.primary, for: .normal)
+        $0.titleLabel?.font = .body2_sb16
+        $0.layer.borderWidth = 1
+        $0.layer.cornerRadius = 12
+        $0.layer.borderColor = UIColor.black.cgColor
+        $0.snp.makeConstraints {
+            $0.height.equalTo(56)
+        }
+    }
     
-    private let taxiSelectionSection = SectionView(title: "차량 선택", subtitle: "상황에 최적화 된 차량과 기사님을 만나보세요\n가장 훌륭한 탑승 경험을 누릴 수 있어요", content: UIView())
+    private let expectedPaymentLabel = UILabel().then {
+        $0.font = .caption_m12
+        $0.textColor = .sub1
+        $0.attributedText = "₩ 15,000 - 19,000".replaceFont(pattern: "[0-9]", replaceFont: .body2_sb16)
+    }
+    
+    private lazy var directPaymentView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.alignment = .center
+        $0.distribution = .fill
+        $0.spacing = 4
+        
+        let cardImageView = UIImageView(image: UIImage(resource: .card)).then {
+            $0.contentMode = .scaleAspectFit
+            $0.setContentHuggingPriority(.required, for: .horizontal)
+            $0.snp.makeConstraints { $0.width.equalTo(36) }
+        }
+        
+        let paymentLabel = UILabel().then {
+            $0.text = "직접 결제"
+            $0.font = .body2_eb16
+            $0.textColor = .primary
+            $0.setContentHuggingPriority(.defaultLow, for: .horizontal)
+            $0.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        }
+        
+        let rightIconImageView = UIImageView(image: UIImage(resource: .rightIcon)).then {
+            $0.contentMode = .scaleAspectFit
+            $0.setContentHuggingPriority(.required, for: .horizontal)
+            $0.snp.makeConstraints { $0.width.height.equalTo(24) }
+        }
+        
+        $0.addArrangedSubviews(cardImageView, paymentLabel, rightIconImageView)
+    }
+    
+    // ScrollView
     
     private let scrollView = UIScrollView().then {
         $0.showsVerticalScrollIndicator = false
     }
     
+    // Sections containing content
+    
+    private lazy var startAndArriveSection = SectionView(
+        title: "출발/도착",
+        content: imageView,
+        contentEdge: .init(top: 10, left: 10, bottom: 10, right: 10)
+    )
+    
+    private lazy var pickupTimeSection = SectionView(
+        title: "픽업 시간",
+        content: pickupTimeLabel,
+        contentEdge: .init(top: 0, left: 25, bottom: 10, right: 25)
+    )
+    
+    private lazy var expectedArriveSection = SectionView(
+        title: "예정 도착 시간",
+        content: expectedArriveStack,
+        contentEdge: .init(top: 0, left: 25, bottom: 10, right: 25)
+    )
+    
+    private lazy var vehicleSelectionSection = SectionView(
+        title: "차량 선택",
+        subtitle: .init(string: "상황에 최적화 된 차량과 기사님을 만나보세요\n가장 훌륭한 탑승 경험을 누릴 수 있어요"),
+        content: vehicleSelectionButton,
+        contentEdge: .init(top: 6, left: 17.5, bottom: 6, right: 17.5)
+    )
+    
+    private lazy var expectedPaymentAccountSection = SectionView(
+        title: "예상 결제 금액",
+        subtitle: .init(string: "적용 가능한 할인 혜택이 없습니다.")
+            .prependImage(image: UIImage(resource: .promotion), imageSize: .init(width: 18, height: 18)),
+        content: expectedPaymentLabel,
+        contentEdge: .init(top: 0, left: 25, bottom: 10, right: 25)
+    )
+    
+    private lazy var directPaymentSection = SectionView(
+        title: "",
+        content: directPaymentView,
+        contentEdge: .init(top: 10, left: 18, bottom: 10, right: 18)
+    )
+    
+    
+    // Container containing sections
+    
     private lazy var contentStackView = UIStackView().then {
         $0.backgroundColor = .bgGray
         $0.axis = .vertical
         $0.spacing = 8
-        
+        expectedPaymentAccountSection.headerAxis = .horizontal
         $0.addArrangedSubviews(startAndArriveSection,
                                pickupTimeSection,
                                expectedArriveSection,
-                               taxiSelectionSection)
+                               vehicleSelectionSection,
+                               expectedPaymentAccountSection,
+                               directPaymentSection)
+        
+        $0.setCustomSpacing(0, after: pickupTimeSection)
     }
 }
 
@@ -52,6 +161,7 @@ final class ReservationInfoViewController: UIViewController {
 extension ReservationInfoViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         addSubViews()
         setLayout()
     }
@@ -62,9 +172,7 @@ extension ReservationInfoViewController {
 extension ReservationInfoViewController {
     private func addSubViews() {
         scrollView.addSubview(contentStackView)
-        [scrollView].forEach {
-            view.addSubview($0)
-        }
+        view.addSubview(scrollView)
     }
     
     private func setLayout() {
--- Uber-iOS/Uber-iOS/Presentation/Reservation/SectionView.swift
+++ Uber-iOS/Uber-iOS/Presentation/Reservation/SectionView.swift
@@ -11,33 +11,40 @@ import SnapKit
 
 final class SectionView: UIView {
     
+    var headerAxis: NSLayoutConstraint.Axis {
+        get {
+            topContainerView.axis
+        } set {
+            topContainerView.axis = newValue
+            titleLabel.setContentHuggingPriority(.required, for: .horizontal)
+        }
+    }
+    
     private let titleLabel = UILabel().then {
         $0.setLabel(alignment: .left, textColor: .primary, font: .body1_eb18)
     }
+    
     private let subtitleLabel = UILabel().then {
         $0.setLabel(alignment: .left, textColor: .sub2, font: .caption_m12)
     }
     
     private let topContainerView = UIStackView().then {
         $0.axis = .vertical
-        $0.alignment = .leading
         $0.spacing = 4.5
     }
     
-    private let bottomContainer = UIStackView().then {
-        $0.axis = .vertical
-    }
+    private let bottomContainer = UIView()
     
     private let contentView = UIStackView().then {
         $0.axis = .vertical
     }
     
-    init(title: String, subtitle: String?, content: UIView, contentEdge: UIEdgeInsets) {
+    init(title: String, subtitle: NSMutableAttributedString?, content: UIView, contentEdge: UIEdgeInsets) {
         super.init(frame: .zero)
         setupView(title: title, subtitle: subtitle, content: content, contentEdge: contentEdge)
     }
     
-    convenience init(title: String, subtitle: String?, content: UIView) {
+    convenience init(title: String, subtitle: NSMutableAttributedString?, content: UIView) {
         self.init(title: title, subtitle: subtitle, content: content, contentEdge: UIEdgeInsets())
     }
     
@@ -53,10 +60,12 @@ final class SectionView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setupView(title: String, subtitle: String?, content: UIView, contentEdge: UIEdgeInsets) {
+    private func setupView(title: String, subtitle: NSMutableAttributedString?, content: UIView, contentEdge: UIEdgeInsets) {
         backgroundColor = .white
         titleLabel.text = title
-        subtitleLabel.text = subtitle
+        subtitleLabel.attributedText = subtitle
+        
+        // Set topContainer
         
         if subtitle != nil {
             topContainerView.addArrangedSubviews(titleLabel, subtitleLabel)
@@ -67,17 +76,36 @@ final class SectionView: UIView {
         topContainerView.isLayoutMarginsRelativeArrangement = true
         topContainerView.layoutMargins = UIEdgeInsets(top: 10, left: 25, bottom: 10, right: 25)
         
-        bottomContainer.addArrangedSubview(content)
-        bottomContainer.isLayoutMarginsRelativeArrangement = true
-        bottomContainer.layoutMargins = contentEdge
-        bottomContainer.layoutMargins.bottom += 10
+        // Set bottomContainer
+        
+        bottomContainer.addSubview(content)
+        
+        content.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(contentEdge.left)
+            $0.top.equalToSuperview().inset(contentEdge.top + 10)
+            $0.bottom.equalToSuperview().inset(contentEdge.bottom + 10)
+            $0.trailing.equalToSuperview().inset(contentEdge.right)
+        }
+        
+        // Set Container
         
         addSubview(contentView)
-        
         contentView.addArrangedSubviews(topContainerView, bottomContainer)
-        
+        topContainerView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+        }
         contentView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
+    }
+}
+
+
+extension NSMutableAttributedString {
+    func appendImage(_ image: UIImage) {
+        let imageAttrachment = NSTextAttachment()
+        imageAttrachment.image = image
+        let strImage = NSAttributedString(attachment: imageAttrachment)
+        self.append(strImage)
     }
 }
