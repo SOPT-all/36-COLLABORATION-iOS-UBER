5193ed396c93b87001f3723a0bd2bfa2c10128a2
SunyoungJu <sts07190@naver.com>
2025-05-15 00:33:50 +0900

    [Feat] UIbutton 익스텐션 추가 (#8)

---
+++ Uber-iOS/Uber-iOS/Global/Extension/UIButton+.swift
@@ -0,0 +1,28 @@
+//
+//  UIButton+.swift
+//  Uber-iOS
+//
+//  Created by 선영주 on 5/14/25.
+//
+import UIKit
+
+extension UIButton {
+
+    enum UberStyle {
+        case blackMain
+    }
+
+    func applyUberStyle(_ style: UberStyle) {
+        switch style {
+        case .blackMain:
+            self.do {
+                $0.backgroundColor = UIColor.btnActive
+                $0.setTitleColor(.white, for: .normal)
+                $0.titleLabel?.font = .body2_sb16
+                $0.layer.cornerRadius = 8
+                $0.clipsToBounds = true
+                $0.contentEdgeInsets = UIEdgeInsets(top: 14, left: 20, bottom: 14, right: 20)
+            }
+        }
+    }
+}
