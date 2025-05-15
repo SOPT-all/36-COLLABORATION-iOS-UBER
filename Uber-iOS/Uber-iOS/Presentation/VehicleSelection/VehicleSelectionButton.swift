764415101c27b4e155fd670d75cf67fa2af46ec1
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-05-16 02:01:46 +0900

    [Add] VehicleSelectionButton 추가 (#17)

---
+++ Uber-iOS/Uber-iOS/Presentation/VehicleSelection/VehicleSelectionButton.swift
@@ -0,0 +1,20 @@
+//
+//  VehicleSelectionButton.swift
+//  Uber-iOS
+//
+//  Created by 권석기 on 5/16/25.
+//
+
+import UIKit
+
+final class VehicleSelectionButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
