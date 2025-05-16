7ad7610c3aa4cefca49f6e97d79a8b75e9df3ad4
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-05-16 16:25:21 +0900

    [Feat] ScrollView 추가 (#12)

--- Uber-iOS/Uber-iOS/Presentation/Reservation/VehicleSelectionViewController.swift
+++ Uber-iOS/Uber-iOS/Presentation/Reservation/VehicleSelectionViewController.swift
@@ -9,10 +9,48 @@ import UIKit
 
 import SnapKit
 
-class VehicleSelectionViewController: BaseViewController {
+final class VehicleSelectionViewController: BaseViewController {
+    
+    // MARK: - Properties
+    
+    private let scrollView = UIScrollView().then {
+        $0.showsVerticalScrollIndicator = false
+    }
+    
+    private lazy var contentStackView = UIStackView().then {
+        $0.backgroundColor = .bgGray
+        $0.axis = .vertical
+        $0.spacing = 8
+    }
+    
+    // MARK: - Layout
+    
+    override func configure() {
+        scrollView.addSubview(contentStackView)
+        addSubviews(scrollView)
+    }
+    
+    override func setConstraints() {
+        scrollView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        contentStackView.snp.makeConstraints {
+            $0.edges.equalTo(scrollView.contentLayoutGuide)
+            $0.width.equalTo(scrollView.frameLayoutGuide)
+        }
+    }
+}
 
+// MARK: - LifeCycle
+
+extension VehicleSelectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
 
+#Preview {
+    VehicleSelectionViewController()
 }
