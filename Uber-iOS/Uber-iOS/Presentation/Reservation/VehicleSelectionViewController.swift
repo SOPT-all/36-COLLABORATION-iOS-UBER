81da4b581474d62fe78ce719b8704f8f5c24c649
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-05-16 16:13:38 +0900

    [Add] VehicleViewController 추가 (#13)

---
+++ Uber-iOS/Uber-iOS/Presentation/Reservation/VehicleSelectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  VehicleSelectionViewController.swift
+//  Uber-iOS
+//
+//  Created by 권석기 on 5/16/25.
+//
+
+import UIKit
+
+import SnapKit
+
+class VehicleSelectionViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
