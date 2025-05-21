8f7397b6adac3d948ec56b4873d3916fc86fc65e
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-05-22 01:21:26 +0900

    [Fix] SceneDelegate 수정 (#41)

--- Uber-iOS/Uber-iOS/Application/SceneDelegate.swift
+++ Uber-iOS/Uber-iOS/Application/SceneDelegate.swift
@@ -23,7 +23,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
 
         let window = UIWindow(windowScene: windowScene)
-        let nav = UINavigationController(rootViewController: ReservationInfoViewController())
+        let nav = UINavigationController(rootViewController: HomeViewController())
         window.rootViewController = nav
         window.makeKeyAndVisible()
         self.window = window                
